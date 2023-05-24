276251059cd00dca8f56e58dfed1fca2dceb925f
ooovvvccc <le.serein@outlook.com>
2023-05-24 03:53:12 +0000

    m

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Flutter App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Flutter App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.network(
+                'https://picsum.photos/250?image=9',
+                width: 250,
+                height: 250,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Hello, Flutter!',
+                style: TextStyle(fontSize: 24),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
